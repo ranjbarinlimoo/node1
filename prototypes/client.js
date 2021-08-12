22ae4882d646e6465fbc27fbfb2dd8a72b4472f9
MohammadReza Ranjbar <ranjbar@parsdata.com>
2021-08-12 18:41:51 +0430

    Add Client.js with GRPC methods

--- prototypes/client.js
+++ prototypes/client.js
@@ -2,21 +2,64 @@ const grpc = require('@grpc/grpc-js')
 const loader = require('@grpc/proto-loader')
 
 
-const pkgDef = loader.loadSync('C:\\Users\\Mamali\\Desktop\\JS\\Limoo\\second_project\\prototypes\\ranjbar.proto',
-    {
-        keepCase: false,
-        longs: String,
-        enums: String,
-        defaults: true,
-        oneofs: true
+const pkgDef = loader.loadSync('PATH/TO/PROTO_FILE',
+//TODO Edit                                 /\this
+    { keepCase: false, longs: String, enums: String, defaults: true, oneofs: true
     }
 )
 
-const pkg = grpc.loadPackageDefinition(pkgDef).AuthPkg
+const pkg = grpc.loadPackageDefinition(pkgDef).Frontend
+const client = new pkg.CustomerPanel('GRPC_SERVER', grpc.credentials.createInsecure())
+//TODO Edit                             /\this
 
-const client = new pkg.TestService('127.0.0.1:50052', grpc.credentials.createInsecure())
 
-client.RegisterSession({UserName:'SSSS',Ip:'192.xx.xx.xx'},function(err,res) {
-    console.log(err)
-    console.log(JSON.parse(res.Data))
+
+//APIs With Examples
+//                    Uncomment log codes for see the results in object format.
+
+client.GetUserContractsList({
+    JSON: JSON.stringify({ UserName: 'moharrami' })},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
+})
+
+
+client.GetContractById({
+    JSON: JSON.stringify({ contractId: '1' })},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
+})
+
+
+client.GetServiceProviderByContractId({
+    JSON: JSON.stringify({ contractId: '1' })},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
+})
+
+
+client.AddServiceProviderRateByContractId({
+    JSON: JSON.stringify({ contractId: '1' , rate: 2 , description: 'A lazy person.' })},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
+})
+
+
+client.EditMyFavoritesByContractId({
+    JSON: JSON.stringify({ contractId: '1' , isMyFavorite: true})},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
+})
+
+
+client.IsMyFavoriteByContractId({
+    JSON: JSON.stringify({ contractId: '1' })},(err,res) => {
+
+    const data = JSON.parse(res.JSON)
+    // console.log(data);
 })
