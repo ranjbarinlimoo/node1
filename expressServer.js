b66a83e6653933cece5c186df3707958eee5d352
MohammadReza Ranjbar <ranjbar@parsdata.com>
2021-08-14 20:05:07 +0430

    Convert gRPC to REST

--- expressServer.js
+++ expressServer.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routers);
 app.use(function (req, res, next) {
